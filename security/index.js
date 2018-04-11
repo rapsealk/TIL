59a0be92fc133c58065a66cc6a86713761619bd8
rapsealk-islab <piono623@naver.com>
2018-04-11 17:48:14 +0900

    .

--- security/index.js
+++ security/index.js
@@ -1,14 +1,30 @@
 const crypto = require('crypto-js');
 
+/**
+ * Cryptograph
+ * @author rapsealk
+ * Symmetric-Key Algorithm: AES (Advanced Encryption Standard)
+ * Asymmetric-Key Algorithm: ECC (Elliptic Curve Cryptography)
+ * Hash Algorithm: SHA-256 (Secure Hash Algorithm)
+ */
+
 const studentNumber = '2015125005';
 const studentName = 'KANGJUNGSUK';
 
-const plainText = 'ABCDEFGHIJKLMNOP';
+const plainText = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const secretKey = studentNumber.split('').map(number => (number | 0x30).toString(2).padStart(8, '0'));
+// Generate Secret Key
+const hashKey = studentNumber.split('').map(number => (number | 0x30).toString(2).padStart(8, '0'));
+studentName.split('').forEach(char => hashKey.push(char.toString(2).padStart(8, '0')));
+console.log('hashKey:', hashKey.join(''));
 
-studentName.split('').forEach(char => secretKey.push(char.toString(2).padStart(8, '0')));
+// Hash plain text
+const hashedMessage = crypto.SHA256(plainText, hashKey).toString();
+console.log('hashedMessage:', hashedMessage);
 
+
+
+/*
 const cipherText = crypto.AES.encrypt(plainText, secretKey.join('')).toString();
 console.log('cipherText:', cipherText);
 
@@ -24,3 +40,4 @@ console.log('cipherText2:', cipherText2);
 console.log('cipherText1:', cipherText.split('').map(c => c.charCodeAt(0).toString(2).padStart(8, '0')).join(' '));
 console.log('cipherText2:', cipherText2.split('').map(c => c.charCodeAt(0).toString(2).padStart(8, '0')).join(' '));
 console.log('cipherText2:', cipherText.split('').map((c, i) => c == cipherText2.charAt(i)).join(' '));
+*/
