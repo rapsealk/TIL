218fd4587f57b78da4adf1dc70bdd69b9d2f1426
rapsealk <piono623@naver.com>
2018-05-14 13:08:56 +0900

    [signal] code on work

--- digital-signal-process/spectrogram-cnn-test.py
+++ digital-signal-process/spectrogram-cnn-test.py
@@ -45,45 +45,65 @@ def max_pool_2x2(x):
 
 DEPTH = 4
 
-x = tf.placeholder(tf.float32, shape=[480, 640, DEPTH])
+x = tf.placeholder(tf.float32, shape=[480, 640, DEPTH], name='x')
 # x = tf.placeholder(tf.float32, shape=[129, 129])
-y = tf.placeholder(tf.float32, shape=[None, len(EMOTIONS)])
+y = tf.placeholder(tf.float32, shape=[None, len(EMOTIONS)], name='y')
 
-x_spectrogram = tf.reshape(x, [-1, 480, 640, DEPTH])
+x_spectrogram = tf.reshape(x, [-1, 480, 640, DEPTH], name='x_spectrogram')
 # x_spectrogram = tf.reshape(x, [-1, 129, 129, 1])
 
-W_conv1 = weight_variable([2, 2, DEPTH, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = tf.Variable(tf.truncated_normal([2, 2, DEPTH, 32], stddev=0.1), name='W_conv1')
+b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]), name='b_conv1')
+# W_conv1 = weight_variable([2, 2, DEPTH, 32])
+# b_conv1 = bias_variable([32])
 
-h_conv1 = tf.nn.relu(conv2d(x_spectrogram, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+h_conv1 = tf.nn.relu(tf.nn.conv2d(x_spectrogram, W_conv1, strides=[1, 2, 2, 1], padding='SAME', name='conv2d_1') + b_conv1, name='h_conv1')
+h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='h_pool1')
+# h_conv1 = tf.nn.relu(conv2d(x_spectrogram, W_conv1) + b_conv1)
+# h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([2, 2, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = tf.Variable(tf.truncated_normal([2, 2, 32, 64], stddev=0.1), name='W_conv2')
+b_conv2 = tf.Variable(tf.constant(0.1, shape=[2, 2, 32, 64]), name='b_conv2')
+# W_conv2 = weight_variable([2, 2, 32, 64])
+# b_conv2 = bias_variable([64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_conv2 = tf.nn.relu(tf.nn.conv2d(x_spectrogram, W_conv2, strides=[1, 2, 2, 1], padding='SAME', name='conv2d_2') + b_conv2, name='h_conv2')
+h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='h_pool2')
+# h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+# h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([30 * 40 * 64, 1024])
-b_fc1 = bias_variable([1024])
+W_fc1 = tf.Variable(tf.truncated_normal([30 * 40 * 64, 1024], stddev=0.1), name='W_fc1')
+b_fc1 = tf.Variable(tf.constant(0.1, [1024]), name='b_fc1')
+# W_fc1 = weight_variable([30 * 40 * 64, 1024])
+# b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 30 * 40 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_pool2_flat = tf.reshape(h_pool2, [-1, 30 * 40 * 64], name='h_pool2_flat')
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name='h_fc1')
+# h_pool2_flat = tf.reshape(h_pool2, [-1, 30 * 40 * 64])
+# h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # dropout
 keep_prob = tf.placeholder(tf.float32, name="keep_prob")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob, name='h_fc1_drop')
+# h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # softmax
-W_fc2 = weight_variable([1024, len(EMOTIONS)])
-b_fc2 = bias_variable([len(EMOTIONS)])
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+W_fc2 = tf.Variable(tf.truncated_normal([1024, len(EMOTIONS)], stddev=0.1), name='W_fc2')
+b_fc2 = tf.Variable(tf.constant(0.1, [len(EMOTIONS)]), name='b_fc2')
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y_conv')
+# W_fc2 = weight_variable([1024, len(EMOTIONS)])
+# b_fc2 = bias_variable([len(EMOTIONS)])
+# y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # train and evaluation
 cross_entropy = -1 * tf.reduce_sum(y * tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1), name='correct_prediction')
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# cross_entropy = -1 * tf.reduce_sum(y * tf.log(y_conv))
+# train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+# correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # session
 sess = tf.Session()
